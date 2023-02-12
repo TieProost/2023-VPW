feba82e2eee0902f349f743d709f6376625f5f10
TieProost <100168771+TieProost@users.noreply.github.com>
2023-02-12 13:38:26 +0100

    _

--- testing_script.py
+++ testing_script.py
@@ -1,8 +1,11 @@
 import subprocess
+import sys
 
-opgave = "2018/cat2/fracking"
+opgave = "2018/cat2/fracking" if len(sys.argv) == 1 else sys.argv[1]
+
 wedstrijd_invoer = opgave + "/wedstrijd.invoer"
 wedstrijd_uitvoer = opgave + "/wedstrijd.uitvoer"
+
 oplossing_script = opgave + "/oplossing.py"
 oplossing_uitvoer = opgave + "/oplossing.uitvoer"
 
@@ -19,14 +22,16 @@ subprocess.Popen(f"python {oplossing_script}",
 
 # bereken punten
 
-juist = totaal = 0
 expected = open(wedstrijd_uitvoer, "r")
-oplossing = open(oplossing_uitvoer, "r")
+resultaat = open(oplossing_uitvoer, "r")
 
-for line in expected.readlines():
-    if oplossing.readline() == line:
+juist = 0
+for i, line in enumerate(expected.readlines()):
+    if line == resultaat.readline():
         juist += 1
-    totaal += 1
 
-percentage = int((juist/totaal)*100)
-print(f"\n{percentage}% = {150 if percentage == 100 else percentage} punten\n")
+# print resultaat
+
+percentage = int((juist/(i+1))*100)
+print(f"\n{opgave}")
+print(f"\n{opgave}\n{percentage}% = {150 if percentage == 100 else percentage} punten\n")
