a7642ff30cd5ddb2a2a1aac4095fbceb41badc81
TieProost <100168771+TieProost@users.noreply.github.com>
2023-02-15 18:41:21 +0100

    oplossing refactor

--- 2017/cat1/loodramen-generatie/oplossing.py
+++ 2017/cat1/loodramen-generatie/oplossing.py
@@ -1,17 +1,16 @@
 for i in range(int(input())):
 
-    rijen_kolommen = input().split(" ")
-    rijen = int(rijen_kolommen[0])
-    kolommen = int(rijen_kolommen[1])
+    dimensies = input().split(" ")
 
-    array = [["." for i in range(kolommen)] for i in range(rijen)]
+    rijen = int(dimensies[0])
+    kolommen = int(dimensies[1])
+    array = [["." for _ in range(kolommen)] for _ in range(rijen)]
 
-    kruispunten = []
-    regel = input().split(" ")
-    for j in range(0, int(regel[0])*2, 2):
+    kruisptn = input().split(" ")
 
-        kruispunt_x = int(regel[j+1]) - 1
-        kruispunt_y = int(regel[j+2]) - 1
+    for j in range(int(kruisptn[0])):
+        kruispunt_x = int(kruisptn[j*2+1]) - 1
+        kruispunt_y = int(kruisptn[j*2+2]) - 1
         array[kruispunt_x][kruispunt_y] = "*"
 
         for x, y in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
@@ -21,9 +20,8 @@ for i in range(int(input())):
             while -1 < punt_x < len(array) and -1 < punt_y < len(array[0]):
                 if array[punt_x][punt_y] != "*":
                     array[punt_x][punt_y] = "*"
-                elif array[punt_x][punt_y] == "*":
+                else:
                     break
-
                 punt_x += x
                 punt_y += y
 
