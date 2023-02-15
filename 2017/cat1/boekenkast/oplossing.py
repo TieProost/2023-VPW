a80c3c86969811904892ef7e84cdfefe6857bdfe
TieProost <100168771+TieProost@users.noreply.github.com>
2023-02-15 23:45:06 +0100

    refactor

--- 2017/cat1/boekenkast/oplossing.py
+++ 2017/cat1/boekenkast/oplossing.py
@@ -1,8 +1,4 @@
 def min_rekken(boeken, rekken):
-    rekken = [int(x) for x in rekken]
-    rekken.sort()
-    rekken.reverse()
-    boeken.sort()
 
     if (len(boeken) == 0):
         return 0
@@ -10,26 +6,32 @@ def min_rekken(boeken, rekken):
     if (len(rekken) == 0):
         return "ONMOGELIJK"
 
-    i_rek = 0
-
+    rek_i = 0
     for boek in boeken:
         breedte = int(boek.split(',')[1])
-        while i_rek < len(rekken) - 1 and int(rekken[i_rek]) < breedte:
-            i_rek += 1
-        if int(rekken[i_rek]) >= breedte:
-            rekken[i_rek] = rekken[i_rek] - breedte
+
+        while rek_i < len(rekken) - 1 and int(rekken[rek_i]) < breedte:
+            rek_i += 1
+
+        if int(rekken[rek_i]) >= breedte:
+            rekken[rek_i] = rekken[rek_i] - breedte
         else:
             return "ONMOGELIJK"
 
-    return i_rek+1
+    return rek_i+1
 
 
 for i in range(int(input())):
-    boeken = []
-    rekken = input().split(" ")[1:]
 
+    rekken = input().split(" ")[1:]
+    rekken = [int(x) for x in rekken]
+    rekken.sort()
+    rekken.reverse()
+
+    boeken = []
     for j in range(int(input())):
         nieuw_boek = input().split(" ")
         boeken += [f"{nieuw_boek[1]},{nieuw_boek[0]}"]
+    boeken.sort()
 
     print(i+1, min_rekken(boeken, rekken))
