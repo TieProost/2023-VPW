fe210ea09373f03de96285c2b53470e3065033e8
Pieter Vandewalle <67506292+PieterVandewalle@users.noreply.github.com>
2023-02-17 14:43:19 +0100

    opl 2018/cat2/slinger verbeterd

--- 2018/cat2/slinger/oplossing.py
+++ 2018/cat2/slinger/oplossing.py
@@ -1,42 +1,26 @@
 import math
 
-
 def zoekPatroon(slinger):
-    begin = huidig = slinger.index("*")
+    aantalDots = lengteSlinger // 5
 
-    dic = {}
-    while (3 not in dic.values()):
-
-        huidig += 1
-        while (huidig < len(slinger) and slinger[huidig] != "*"):
-            huidig += 1
-
-        interval = huidig - begin
-        begin = huidig
-
-        dic[interval] = dic[interval] + 1 if interval in dic else 1
-
-    return "*" + "."*(interval-1)
-
+    for aantal in range(aantalDots,0,-1):
+        if slinger.count("." * aantal) >= 3:
+            return "*" + "." * aantal
 
 def corrigeer(slinger):
     patroon = zoekPatroon(slinger)
     eersteIndex = slinger.index(patroon)
 
-    start = ""
-    if eersteIndex != 0:
-        start = patroon[len(patroon) - eersteIndex:]
-
-    aantalKeer = math.ceil(len(slinger) / len(patroon))
+    start = patroon[len(patroon) - eersteIndex:]
+    aantalKeer = math.ceil(lengteSlinger / len(patroon))
     res = start + (patroon * aantalKeer)
 
-    return res[:len(slinger)]
-
+    return res[:lengteSlinger]
 
 aantalTestGevallen = int(input())
 
 for i in range(aantalTestGevallen):
-    lengte = int(input())
+    lengteSlinger = int(input())
     slinger = input()
     correct = corrigeer(slinger)
     print(f"{i+1} {correct}")
